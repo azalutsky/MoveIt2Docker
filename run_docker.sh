7603ef6b6bc4a1075a0aa7124cfa9f9dce496921
Anton Zalutsky <azalutsky@gmail.com>
2022-10-14 00:35:33 -0700

    simplify

--- run_docker.sh
+++ run_docker.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Please set $DENALI_WS_DIR to your local ebots_ros2_arms workspace
-# Example: DENALI_WS_DIR=/home/USER/ws/ebots/ebots_ros2_arms
-
 xhost +local:
-export WORK_DIR=/home/ebots
+export WORK_DIR=/home/blah
 docker run -it --rm \
     --network host \
-    --privileged \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
-    -v $DENALI_WS_DIR/workspace/motoman_motomini_support:$WORK_DIR/workspace/motoman_motomini_support \
     -w $WORK_DIR \
     moveit2-tutorial \
     /bin/bash -ci "source /root/.bashrc && /bin/bash"
